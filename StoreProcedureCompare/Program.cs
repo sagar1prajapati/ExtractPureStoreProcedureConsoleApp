0fdb5e3554a35f8ea4675a3b8bdbcc342b9fcccc
sagar1prajapati <sagarprajapati2010@gmail.com>
2021-08-07 21:37:02 +0530

    Add Compare Features

--- StoreProcedureCompare/Program.cs
+++ StoreProcedureCompare/Program.cs
@@ -10,10 +10,21 @@ namespace StoreProcedureCompare
     class Program
     {
         static readonly string textFile = @"c:\users\sagar\documents\visual studio 2015\Projects\StoreProcedureCompare\StoreProcedureCompare\SP.txt";
+        static readonly string textFileV1 = @"c:\users\sagar\documents\visual studio 2015\Projects\StoreProcedureCompare\StoreProcedureCompare\SP_V1.txt";
         static void Main(string[] args)
         {
           string Procedure  = ExtractProcedure.ExtractPureProcedure(textFile);
-            Console.WriteLine(Procedure);
+          string Procedure2 = ExtractProcedure.ExtractPureProcedure(textFileV1);
+
+            if (String.Equals(Procedure, Procedure2))
+            {
+                Console.WriteLine("Procedures are Same");
+            }else
+            {
+                Console.WriteLine("Differance Found");
+            }
+
+                //Console.WriteLine(Procedure);
             Console.ReadKey();
         }
     }
