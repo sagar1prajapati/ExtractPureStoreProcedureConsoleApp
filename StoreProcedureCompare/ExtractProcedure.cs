ec55a1c017f22d5e38b440785ec02b3ae45f68ec
sagar1prajapati <sagarprajapati2010@gmail.com>
2021-08-07 21:12:44 +0530

    Add project files.

---
+++ StoreProcedureCompare/ExtractProcedure.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace StoreProcedureCompare
+{
+    public class ExtractProcedure
+    {
+        public static string  ExtractPureProcedure(string textFile)
+        {
+            string OnlyPureProcedure = null;
+            if (File.Exists(textFile))
+            {
+                // Read entire text file content in one string    
+                string Procedure = File.ReadAllText(textFile);
+                Procedure = Procedure.ToUpper();
+                Procedure = Procedure.Trim();
+                string firstLetter = ExtractProcedure.FindFirstWord(Procedure);
+                int EndIndex = FindIndexOfWord(Procedure, firstLetter);
+                //Console.WriteLine("Create Procedure Index " + EndIndex);
+                //Console.WriteLine("First World is" + firstLetter);
+                OnlyPureProcedure = ExtractOnlyProcedureTextFromRawProcedure(Procedure, EndIndex);
+               
+            }
+            return OnlyPureProcedure;
+        }
+
+
+        public static string FindFirstWord(string text)
+        {
+            string FirstWorld = null;
+            if (text.Contains("CREATE PROCEDURE"))
+            {
+                FirstWorld = "CREATE PROCEDURE";
+            }
+            else if (text.Contains("CREATE PROC"))
+            {
+                FirstWorld = "CREATE PROC";
+            }
+            else if (text.Contains("ALTER PROCEDURE"))
+            {
+                FirstWorld = "ALTER PROCEDURE";
+            }
+            else if (text.Contains("ALTER PROC"))
+            {
+                FirstWorld = "ALTER PROC";
+            }
+            return FirstWorld;
+        }
+        public static int FindIndexOfWord(string fullProcedure, string text)
+        {
+            int ProcedureStartingCharacterIndex = fullProcedure.IndexOf(text);
+            return ProcedureStartingCharacterIndex;
+        }
+
+        public static string ExtractOnlyProcedureTextFromRawProcedure(string RawProcedure, int startIndex)
+        {
+            int EndIndex = RawProcedure.Length;
+            int length = RawProcedure.Length - startIndex;
+            string extracted = RawProcedure.Substring(startIndex, length);
+            return extracted;
+        }
+    }
+}
---
+++ StoreProcedureCompare/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreProcedureCompare
+{
+    class Program
+    {
+        static readonly string textFile = @"c:\users\sagar\documents\visual studio 2015\Projects\StoreProcedureCompare\StoreProcedureCompare\SP.txt";
+        static void Main(string[] args)
+        {
+          string Procedure  = ExtractProcedure.ExtractPureProcedure(textFile);
+            Console.WriteLine(Procedure);
+            Console.ReadKey();
+        }
+    }
+}
